2abf4c29cb09837e9c5bb7151bc1e008358122bc
ks <1379945771@qq.com>
2019-09-05 15:29:52 +0800

    threadLocal操作

---
+++ src/main/java/keyWord/ThreadLocalDemo.java
@@ -0,0 +1,55 @@
+package keyWord;
+
+/**
+ * @Description
+ * @Author shichao.chen
+ * @Date 2019/9/5 13:48
+ * @Version 1.0
+ **/
+public class ThreadLocalDemo {
+    ThreadLocal<Long> longLocal = new ThreadLocal<Long>(){
+        protected Long initialValue() {
+            return Thread.currentThread().getId();
+        };
+    };
+    ThreadLocal<String> stringLocal = new ThreadLocal<String>(){;
+        protected String initialValue() {
+            return Thread.currentThread().getName();
+        };
+    };
+
+
+    public void set() {
+        longLocal.set(Thread.currentThread().getId());
+        stringLocal.set(Thread.currentThread().getName());
+    }
+
+    public long getLong() {
+        return longLocal.get();
+    }
+
+    public String getString() {
+        return stringLocal.get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final ThreadLocalDemo test = new ThreadLocalDemo();
+
+        test.set();
+        System.out.println(test.getLong());
+        System.out.println(test.getString());
+
+        Thread thread1 = new Thread(){
+            public void run() {
+                test.set();
+                System.out.println(test.getLong());
+                System.out.println(test.getString());
+            };
+        };
+        thread1.start();
+        thread1.join();
+
+        System.out.println(test.getLong());
+        System.out.println(test.getString());
+    }
+}
