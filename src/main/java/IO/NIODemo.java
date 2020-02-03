63e91f178ab10feb6e3d541537c35ae488cf2b8f
achiever-or-loser <1379945771@qq.com>
2020-02-03 21:35:23 +0800

    NIO demo

---
+++ src/main/java/IO/NIODemo.java
@@ -0,0 +1,113 @@
+package IO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @description:
+ * @author: csc
+ * @create: 2020/1/21 22:47
+ */
+public class NIODemo {
+    public static void fastCopy(String src, String dist) throws IOException {
+        /* 获得源文件的输入字节流 */
+        FileInputStream fin = new FileInputStream(src);
+        /* 获取输入字节流的文件通道 */
+        FileChannel fcin = fin.getChannel();
+        /* 获取目标文件的输出字节流 */
+        FileOutputStream fout = new FileOutputStream(dist);
+        /* 获取输出字节流的文件通道 */
+        FileChannel fcout = fout.getChannel();
+        /* 为缓冲区分配 1024 个字节 */
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+        while (true) {
+            /* 从输入通道中读取数据到缓冲区中 */
+            int r = fcin.read(buffer);
+            /* read() 返回 -1 表示 EOF */
+            if (r == -1) {
+                break;
+            }
+            /* 切换读写 */
+            buffer.flip();
+            /* 把缓冲区的内容写入输出文件中 */
+            fcout.write(buffer);
+            /* 清空缓冲区 */
+            buffer.clear();
+        }
+    }
+}
+
+
+class NIOServer {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel ssChannel = ServerSocketChannel.open();
+        ssChannel.configureBlocking(false);
+        ssChannel.register(selector, SelectionKey.OP_ACCEPT);
+        ServerSocket serverSocket = ssChannel.socket();
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8888);
+        serverSocket.bind(address);
+        while (true) {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = keys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel ssChannel1 = (ServerSocketChannel) key.channel();
+                    // 服务器会为每个新连接创建一个 SocketChannel
+                    SocketChannel sChannel = ssChannel1.accept();
+                    sChannel.configureBlocking(false);
+                    // 这个新连接主要用于从客户端读取数据
+                    sChannel.register(selector, SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    SocketChannel sChannel = (SocketChannel) key.channel();
+                    System.out.println(readDataFromSocketChannel(sChannel));
+                    sChannel.close();
+                }
+                keyIterator.remove();
+            }
+        }
+    }
+
+    private static String readDataFromSocketChannel(SocketChannel sChannel) throws
+            IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        StringBuilder data = new StringBuilder();
+        while (true) {
+            buffer.clear();
+            int n = sChannel.read(buffer);
+            if (n == -1) {
+                break;
+            }
+            buffer.flip();
+            int limit = buffer.limit();
+            char[] dst = new char[limit];
+            for (int i = 0; i < limit; i++) {
+                dst[i] = (char) buffer.get(i);
+            }
+            data.append(dst);
+            buffer.clear();
+        }
+        return data.toString();
+    }
+}
+
+class NIOClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 8888);
+        OutputStream out = socket.getOutputStream();
+        String s = "hello world";
+        out.write(s.getBytes());
+        out.close();
+    }
+}
