4417cb0e56b5cb12cf28645ba24c621c41096d5b
csc <1379945771@qq.com>
2020-07-31 18:20:43 +0800

    工厂模式

---
+++ src/main/java/designPattern/factory/Circle.java
@@ -0,0 +1,12 @@
+package designPattern.factory;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Circle::draw() method.");
+
+	}
+
+}
