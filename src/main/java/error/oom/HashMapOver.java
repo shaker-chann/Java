009c7c39ddac6520147af9a146caed011bf81dce
csc <1379945771@qq.com>
2020-10-23 10:13:33 +0800

    error oom,dead lock

---
+++ src/main/java/error/oom/HashMapOver.java
@@ -0,0 +1,77 @@
+package error.oom;
+
+/**
+ * @Description:
+ * @PackageName: com.csc.demo1.other
+ * @Author: 陈世超
+ * @Create: 2020-09-30 14:24
+ * @Version: 1.0
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapOver {
+
+    public static void main(String[] args) {
+        Map<Person, Integer> map = new HashMap<Person, Integer>();
+        try {
+            Thread.sleep(10000);//给打开visualvm时间
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //循环添加对象到缓存
+        for (int i = 0; i < 4000000; i++) {
+            TestMemory t = new TestMemory();
+
+            Person p = new Person("name" + i, i);
+            map.put(p, i);
+            p.setName(i + "lisi");
+            map.remove(p);
+        }
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("qqqq");
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() * 123;
+    }
+}
---
+++ src/main/java/error/oom/MemoryShake.java
@@ -0,0 +1,55 @@
+package error.oom;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @PackageName: com.csc.demo1.other
+ * @Author: 陈世超
+ * @Create: 2020-09-30 13:35
+ * @Version: 1.0
+ */
+public class MemoryShake {
+    static class OOMClass {
+        private int[] oom = new int[1024 * 100];// 100KB
+    }
+
+    //声明缓存对象
+    private static final Map map = new HashMap();
+
+    public static void main(String args[]) {
+//        ArrayList<String > list = new ArrayList<>();
+//        SoftReference<ArrayList> softRef = new SoftReference<ArrayList>(list);
+        ReferenceQueue<OOMClass> queue = new ReferenceQueue<>();
+        List<WeakReference> list = new ArrayList<>();
+
+        try {
+            Thread.sleep(10000);//给打开visualvm时间
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //循环添加对象到缓存
+        for (int i = 0; i < 4000000; i++) {
+            TestMemory t = new TestMemory();
+            map.put("key" + i, t);
+            list.add(new WeakReference<OOMClass>(new OOMClass(), queue));
+        }
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("qqqq");
+    }
+
+}
+
+class TestMemory {
+    public String name = "huangzs";
+    public String address = "huangzs_jiujiangshi_jiangxisheng";
+}
