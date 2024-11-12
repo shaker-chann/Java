f1860fdbd63b3078cb373904d1ea37468bcba005
csc <1379945771@qq.com>
2024-11-12 15:47:48 +0800

    addTwoNumbers with ListNode

--- src/main/java/LeetCode/collection/ListNodeMain.java
+++ src/main/java/LeetCode/collection/ListNodeMain.java
@@ -24,6 +24,24 @@ public class ListNodeMain {
 
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0), cur = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry == 1) cur.next = new ListNode(carry);
+        return head.next;
+    }
+
     public ListNode sortList(ListNode head) {
         return sortList(head, null);
     }
